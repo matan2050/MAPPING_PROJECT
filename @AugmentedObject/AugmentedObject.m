be9d39ac31fdff34377356de754d9618ecc9fbdf
Matan Weissbuch <matan2050@gmail.com>
2015-08-16 09:08:26 +0300

    Added points and lines creation for AugmentedObject

--- @AugmentedObject/AugmentedObject.m
+++ @AugmentedObject/AugmentedObject.m
@@ -4,7 +4,8 @@ classdef AugmentedObject < handle
     ShapeRadius
     Position
     Points
-    PointsIndicesTopology
+    Lines
+    Face
   end
   
   methods
@@ -48,10 +49,12 @@ classdef AugmentedObject < handle
       % object's points will be 8x3 matrix
       obj.Points = zeros(8,3);
       
-      % assigning new names for object parameter for convenience
+      % Assigning new names for object parameter for convenience
       r = obj.ShapeRadius;
       p = obj.Position;
       
+      
+      % Filling points list
       obj.Points(1,:) = p + [r/2, r/2, r/2]';
       obj.Points(2,:) = p + [r/2, r/2, -r/2]';
       obj.Points(3,:) = p + [r/2, -r/2, r/2]';
@@ -60,6 +63,24 @@ classdef AugmentedObject < handle
       obj.Points(6,:) = p + [-r/2, r/2, -r/2]';
       obj.Points(7,:) = p + [r/2, -r/2, -r/2]';
       obj.Points(8,:) = p + [-r/2, -r/2, -r/2]';
+      
+      
+      % Line-points connectivity
+      obj.Lines = zeros(10,2);
+      obj.Lines(1,:) = [1,2];
+      obj.Lines(2,:) = [1,3];
+      obj.Lines(3,:) = [1,4];
+      obj.Lines(4,:) = [2,6];
+      obj.Lines(5,:) = [2,7];
+      obj.Lines(6,:) = [3,7];
+      obj.Lines(7,:) = [4,6];
+      obj.Lines(8,:) = [5,8];
+      obj.Lines(9,:) = [6,8];
+      obj.Lines(10,:) = [7,8];
+      
+      
+      % Face-lines connectivity
+      obj.
     end
   end
   
