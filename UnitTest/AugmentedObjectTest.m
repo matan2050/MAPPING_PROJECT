68c452c662bdfc12bc38c38ac42c1e2bb809a439
Matan Weissbuch <matan2050@gmail.com>
2015-08-17 08:56:04 +0300

    AugmentedObject testing script

---
+++ UnitTest/AugmentedObjectTest.m
@@ -0,0 +1,16 @@
+%% Test Initialization + faces and lines visualizations
+testObj = AugmentedObject();
+testObj.CreateShape([0;0;0], 50);
+testObj.VisualizeLines3D();
+testObj.VisualizeFaces3D();
+
+
+%% Define camera model and project the shape to it
+model = CameraModel([], 1000, 1000, 1000, 1000, 0, 0, 0, 10, 10, 10, [2000 2000])
+testObj.ShapeToImage(model);
+testObj.VisualizeLines2D();
+testObj.VisualizeFaces2D();
+model.UpdateByRotation(pi/4, pi/4, pi/4);
+testObj.ShapeToImage(model);
+testObj.VisualizeLines2D();
+testObj.VisualizeFaces2D();
